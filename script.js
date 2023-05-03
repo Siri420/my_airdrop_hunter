7ddf016df441391867276d5c161ce40dd28ca834
Siri420 <54490168+Siri420@users.noreply.github.com>
2023-05-03 18:19:03 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+// Setup
 import { Network, Alchemy } from 'alchemy-sdk';
 
 const settings = {
-    apiKey: "2369EK9D4M8hFlrgfk0hPfCoRfji8i3C",
-    network: Network.ETH_GOERLI,
+    apiKey: "JNRxE82ZZVKonzUXlkIfwmPipgdMumDS",
+    network: Network.ETH_SEPOLIA,
 };
 
 const alchemy = new Alchemy(settings);
 
-// get the latest block
-const latestBlock = alchemy.core.getBlock("latest").then(console.log);
+// Get the latest block
+const latestBlock = alchemy.core.getBlockNumber();
+
+// Get all outbound transfers for a provided address
+alchemy.core
+    .getTokenBalances('0x994b342dd87fc825f66e51ffa3ef71ad818b6893')
+    .then(console.log);
+
+// Get all the NFTs owned by an address
+const nfts = alchemy.nft.getNftsForOwner("0xshah.eth");
+
+// Listen to all new pending transactions
+alchemy.ws.on(
+    { method: "alchemy_pendingTransactions",
+    fromAddress: "0xshah.eth" },
+    (res) => console.log(res)
+);
